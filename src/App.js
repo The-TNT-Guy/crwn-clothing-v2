742ea706941a3f3fe75171154103d781e44f469a
Ruben Wolterbeek <ruben.wolterbeek@hva.nl>
2022-09-13 15:49:53 +0200

    directory page done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Women",
-    },
-    {
-      id: 5,
-      title: "Men",
-    },
-  ];
-  return (
-    <div className="categories-container">
-      {categories.map(({ id, title }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+import Directory from "./components/directory/directory.component";
+
+const App = () => (
+  <Directory/>
+);
 
 export default App;
