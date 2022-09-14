fbca2f847e1ef69b386e9792b6c7ea3871f28d9b
Ruben Wolterbeek <ruben.wolterbeek@hva.nl>
2022-09-14 10:46:08 +0200

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => (
-  <Directory/>
+  <Routes>
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+    </Route>
+  </Routes>
 );
 
 export default App;
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,6 @@
+
+const Shop = () => {
+  return <h1>I am the shop</h1>;
+};
+
+export default Shop;
