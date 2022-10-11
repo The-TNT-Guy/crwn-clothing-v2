2ab4f2955667030826ceabd5ea508e743e1122ee
Ruben Wolterbeek <ruben.wolterbeek@hva.nl>
2022-10-11 13:44:37 +0200

    Redux thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import { setCategoriesArray } from "../../store/categories/category.action";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 import CategoriesOverview from "../categories-overview/categories-overview.component";
 import CategoryProducts from "../category-products/category-products.component";
 import "./shop.styles.scss";
@@ -10,13 +9,7 @@ import "./shop.styles.scss";
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    // addCollectionAndDocuments("categories", SHOP_DATA)
-    const getCategories = async () => {
-      const newCategoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategoriesArray(newCategoriesArray));
-    };
-
-    getCategories();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
 
   return (
